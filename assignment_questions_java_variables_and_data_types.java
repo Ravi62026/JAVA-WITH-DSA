e7e396e1dcb6d1652baa72f801e33d79bd223e2c
Ravi62026 <128376014+Ravi62026@users.noreply.github.com>
2023-03-20 21:18:21 +0530

    Add files via upload

---
+++ assignment_questions_java_variables_and_data_types.java
@@ -0,0 +1,77 @@
+// Take 2 integer values in two variables x and y and print their product
+
+//public class product
+//{
+//    public static void main(String[] args)
+//    {
+//        int x = 2;
+//        int y = 4;
+//        int z = x*y;
+//        System.out.println(z);
+//    }
+//}
+
+
+
+//// Print the ASCII value of character ‘U’.
+//
+//public class ascii_value
+//{
+//    public static void main(String[] args)
+//    {
+//        char name = 'U';
+//        int ascciivalue = name;
+//        System.out.println(ascciivalue);
+//    }
+//}
+
+
+
+// Write a Java program to take the length and breadth of a rectangle and print its area.
+
+//public class area_of_rectangle
+//{
+//    public static void main(String[] args)
+//    {
+//        int length = 7;
+//        int breadth = 4;
+//        float area = length * breadth;
+//        System.out.println(area);
+//    }
+//
+//}
+
+
+// Write a Java program to calculate the cube of a number.
+
+//public class cube
+//{
+//    public static void main(String[] args)
+//    {
+//        int a = 4;
+//        float cube = a*a*a;
+//        System.out.println("cube of a is :-" + cube);
+//
+//    }
+//}
+
+
+
+
+// Write a Java program to swap two numbers with the help of a third variable
+
+//public interface swap_two_number
+//{
+//    public static void main(String[] args)
+//    {
+//        int a = 2;
+//        int b = 3;
+//        int c = a;
+//        a = b;
+//        b = c;
+//        System.out.println("value of a is :-" +a);
+//        System.out.println("value of b is :-" +b);
+//    }
+//}
+
+
