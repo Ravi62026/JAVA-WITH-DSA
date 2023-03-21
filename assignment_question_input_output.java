bef7ae45dcaba2bd090cc5661f18343e99842026
Ravi62026 <128376014+Ravi62026@users.noreply.github.com>
2023-03-21 18:58:09 +0530

    Add files via upload

---
+++ assignment_question_input_output.java
@@ -0,0 +1,100 @@
+// Ques1 - Input name, roll number and field of interest from user and print in the format below :
+//Name: xyz, Roll number: xyz, Field of interest: xyz
+
+//import java.util.Scanner;
+//public class assignment_question_input_output
+//{
+//    public static void main(String[] args)
+//    {
+//        Scanner sc = new Scanner(System.in);
+//        String first_name = sc.next();
+//        String last_name = sc.next();
+//        int roll_number = sc.nextInt();
+//        String foi = sc.next();
+//        System.out.println("name:-" +first_name + " " +last_name);
+//        System.out.println("roll number:-" +roll_number);
+//        System.out.println("field of interest:-" +foi);
+//    }
+//}
+
+
+
+
+// Ques2 - Input two different string and print them in same line.
+
+//import java.util.Scanner;
+//public class assignment_question_input_output
+//{
+//    public static void main(String[] args)
+//    {
+//        Scanner sc = new Scanner(System.in);
+//        String s1 = sc.nextLine();
+//        String s2 = sc.nextLine();
+//        System.out.print(s1);
+//        System.out.print(s2);
+//    }
+//}
+//
+
+
+
+
+//  Ques2 - If the marks of Robert in three subjects are entered through keyboard (each out of
+//100), write a program to calculate his total marks and percentage marks.
+
+
+//import java.util.Scanner;
+//public class assignment_question_input_output
+//{
+//    public static void main(String[] args)
+//    {
+//        Scanner sc = new Scanner(System.in);
+//        int marks1 = sc.nextInt();
+//        int marks2 = sc.nextInt();
+//        int marks3 = sc.nextInt();
+//        int total_mark = marks1+marks2+marks3;
+//        float avg_marks = (marks1+marks2+marks3)/3 ;
+//        System.out.println("total marks is:-" +total_mark);
+//        System.out.println("avg marks is:-" +avg_marks);
+//    }
+//}
+//
+
+
+// Ques4 - Given two numbers, return their sum in the following format:
+//Int t representing number of test cases
+//T lines of Two integers representing the numbers to be added
+
+//import java.util.Scanner;
+//public class assignment_question_input_output {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int t = sc.nextInt();
+//        for(int i = 1; i <= t; i++){
+//            int a = sc.nextInt();
+//            int b = sc.nextInt();
+//            System.out.println(a+b);
+//        }
+//    }
+//}
+//
+
+
+
+// Ques5 - Given few lines of input(number of lines unknown) where each line has two
+//strings, concatenate the strings.
+
+
+//import java.util.Scanner;
+//public class assignment_question_input_output {
+//    public static void main(String[] args) {
+//        Scanner scn = new Scanner(System.in);
+//        while(scn.hasNextLine()){
+//            String a = scn.next();
+//            String b = scn.next();
+//            System.out.println(a+b);
+//        }
+//    }
+//}
+//
+
